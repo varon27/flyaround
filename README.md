d10a6094e82413cf17c031e7268c16e9a2594c93
Valeriane Aron <valeriane.aron@gmail.com>
2018-01-15 16:36:00 +0100

    delete project

--- README.md
+++
@@ -1,4 +0,0 @@
-flyaround
-=========
-
-A Symfony project created on December 4, 2017, 2:40 pm.
--- src/WCS/CoavBundle/WCSCoavBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace WCS\CoavBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class WCSCoavBundle extends Bundle
-{
-}
